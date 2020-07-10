2638f915c94c1c59d750eb1fd0b81aebb2796871
Lashonda Miller <lashondamiller007@gmail.com>
2020-07-10 00:08:01 -0400

    practice for loops

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var oreoCookie = [];
+
+for(var i = 1; i<=10; i++) {
+    oreoCookie.push(i)
+}
+console.log(oreoCookie);
+
+//
+
+var bruh = [];
+var i = 10;
+
+while (i>=1) {
+    bruh.push(i);
+    i--;
+}
+console.log(bruh);
+
+//
+
+var meBaby = [];
+for(var i = 11; i <= 21; i+=2) {
+    meBaby.push(i)
+}
+console.log(meBaby);
+
+//
+
+var twinkieMe = [];
+for(var i = 15; i >= 1; i-=2) {
+    twinkieMe.push(i)
+}
+console.log(twinkieMe);
